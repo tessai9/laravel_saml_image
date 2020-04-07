3d20e1f68e697d3b4bbe884e32a9a86dccd51141
tessai9 <shimeji9@gmail.com>
2020-04-07 16:34:17 +0900

    Docker環境の修正

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM php:7.3.11-fpm-alpine
 
+# install php extention pdo_mysql
+RUN docker-php-ext-install pdo_mysql
+
 RUN apk update \
     && apk add wget \
     curl \
@@ -14,3 +17,5 @@ WORKDIR /app
 COPY ./ /app
 
 RUN php composer.phar install
+
+EXPOSE 9000
--- docker/simplesamlphp/config/config.php
+++ docker/simplesamlphp/config/config.php
@@ -461,7 +461,7 @@ $config = [
      * one of the functionalities below, but in some cases you could run multiple functionalities.
      * In example when you are setting up a federation bridge.
      */
-    'enable.saml20-idp' => false,
+    'enable.saml20-idp' => true,
     'enable.shib13-idp' => false,
     'enable.adfs-idp' => false,
 
